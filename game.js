2a18f5bed8dfbed0a9c33434d1b95fd2cec20162
ThatChip <33401420+ThatChip@users.noreply.github.com>
2024-07-22 22:57:40 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+const gameWidth = canvas.width;
+const gameHeight = canvas.height;
+
+let lastTime = 0;
+function gameLoop(timestamp) {
+    const deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.clearRect(0, 0, gameWidth, gameHeight);
+
+    // Update and draw game elements here
+
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
