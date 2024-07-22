7ba62f56413a1907c73c5142668ce11f7de29872
Nounet <little-dawn@hotmail.fr>
2024-07-22 23:09:25 +0200

    adding script.js file

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('createGameButton').addEventListener('click', function() {
+    // This is where the create game functionality will go
+    console.log('Create Game button clicked!');
+});
